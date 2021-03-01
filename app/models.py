6088d22dd5515bbb28999f8c88c53b4da2456488
Martin Du <martindu263@outlook.com>
2021-03-01 11:39:01 +0800

    0301temp

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 from sqlalchemy.sql import func
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +15,13 @@ class User(db.Model):
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
+
+	#有哈希密码的表单添加下列方法
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
